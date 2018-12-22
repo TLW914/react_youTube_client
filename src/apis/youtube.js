ee8bd87fe4f46c6df5c54afb343452fbab756b74
Tyler Watson <leggettwatson.tyler@gmail.com>
2018-12-21 16:26:29 -0800

    initialized component architecture.set up youtube api w/ axios

---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyCfzBxMgfnCieklskEIB3freedaG45y8-w';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3'
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY,
+    }
+});
+
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
+    onTermSubmit = (term) => {
+
+    }
+    
     render(){
         return (
-            <div>Hello World</div>
+            <div className="ui container">
+            <SearchBar onFormSubmission={this.onTermSubmit}/>
+            </div>
         )
     }
 }
